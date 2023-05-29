2c2574798644b43bc40ccbfafeeecf4a76820423
den3606 <dedendendedenpunn@gmail.com>
2023-05-29 21:35:57 +0900

    wip

--- files/scripts/magic/increase_draw_4_next.lua
+++ files/scripts/magic/increase_draw_4_next.lua
@@ -1,6 +1,13 @@
 dofile_once("mods/d-wonders/files/scripts/lib/utilities.lua")
 
-local INCREASE_DRAW_SIZE = 4
+local INCREASE_DRAW = {
+  DRAW_SIZE = 4,
+  EXECUTED_MARK_TAG = "enabled_increase_draw_4",
+  VARIABLE_KEYS = {
+    TRUE_DRAW = "increase_draw_decrease_draw.true_actions_per_round",
+    REIGSTERED_WAND_ENTITY = "increase_draw_4.target_wand_entity_id",
+  },
+}
 
 local function get_gun_config_actions_per_round_ability_component(wand_entity_id)
   if EntityHasTag(wand_entity_id, "wand") then
@@ -12,21 +19,21 @@ local function get_gun_config_actions_per_round_ability_component(wand_entity_id
       end
     end
   end
-  print_error('Wand do not have gun_config.')
+  print_error("Wand do not have gun_config.")
   return nil
 end
 
 local function increase_actions_per_round(spell_entity_id, wand_entity_id, ability_component)
-  EntityAddTag(spell_entity_id, 'enabled_increase_draw_4')
+  EntityAddTag(spell_entity_id, INCREASE_DRAW.EXECUTED_MARK_TAG)
 
-  local true_actions_per_round = GetInternalVariableValue(wand_entity_id, 'increase_draw_decrease_draw.true_actions_per_round', 'value_int')
+  local true_actions_per_round = GetInternalVariableValue(wand_entity_id, INCREASE_DRAW.VARIABLE_KEYS.TRUE_DRAW, "value_int")
   if true_actions_per_round == nil then
     true_actions_per_round = tonumber(ComponentObjectGetValue2(ability_component, "gun_config", "actions_per_round"))
-    AddNewInternalVariable(wand_entity_id, 'increase_draw_decrease_draw.true_actions_per_round', 'value_int', true_actions_per_round)
+    AddNewInternalVariable(wand_entity_id, INCREASE_DRAW.VARIABLE_KEYS.TRUE_DRAW, "value_int", true_actions_per_round)
   end
 
-  local new_actions_per_round = true_actions_per_round + INCREASE_DRAW_SIZE
-  SetInternalVariableValue(wand_entity_id, "increase_draw_decrease_draw.true_actions_per_round", "value_int", new_actions_per_round)
+  local new_actions_per_round = true_actions_per_round + INCREASE_DRAW.DRAW_SIZE
+  SetInternalVariableValue(wand_entity_id, INCREASE_DRAW.VARIABLE_KEYS.TRUE_DRAW, "value_int", new_actions_per_round)
 
   if new_actions_per_round < 1 then
     new_actions_per_round = 1
@@ -35,18 +42,18 @@ local function increase_actions_per_round(spell_entity_id, wand_entity_id, abili
 end
 
 local function decrease_actions_per_round(spell_entity_id, wand_entity_id, ability_component)
-  EntityRemoveTag(spell_entity_id, 'enabled_increase_draw_4')
+  EntityRemoveTag(spell_entity_id, INCREASE_DRAW.EXECUTED_MARK_TAG)
 
-  RemoveInternalVariableValue(spell_entity_id, 'increase_draw_4.target_wand_entity_id')
+  RemoveInternalVariableValue(spell_entity_id, INCREASE_DRAW.VARIABLE_KEYS.REIGSTERED_WAND_ENTITY)
 
-  local true_actions_per_round = GetInternalVariableValue(wand_entity_id, 'increase_draw_decrease_draw.true_actions_per_round', 'value_int')
+  local true_actions_per_round = GetInternalVariableValue(wand_entity_id, INCREASE_DRAW.VARIABLE_KEYS.TRUE_DRAW, "value_int")
   if true_actions_per_round == nil then
     true_actions_per_round = tonumber(ComponentObjectGetValue2(ability_component, "gun_config", "actions_per_round"))
-    AddNewInternalVariable(wand_entity_id, 'increase_draw_decrease_draw.true_actions_per_round', 'value_int', true_actions_per_round)
+    AddNewInternalVariable(wand_entity_id, INCREASE_DRAW.VARIABLE_KEYS.TRUE_DRAW, "value_int", true_actions_per_round)
   end
 
-  local new_actions_per_round = true_actions_per_round - INCREASE_DRAW_SIZE
-  SetInternalVariableValue(wand_entity_id, "increase_draw_decrease_draw.true_actions_per_round", "value_int", new_actions_per_round)
+  local new_actions_per_round = true_actions_per_round - INCREASE_DRAW.DRAW_SIZE
+  SetInternalVariableValue(wand_entity_id, INCREASE_DRAW.VARIABLE_KEYS.TRUE_DRAW, "value_int", new_actions_per_round)
 
   if new_actions_per_round < 1 then
     new_actions_per_round = 1
@@ -56,10 +63,10 @@ local function decrease_actions_per_round(spell_entity_id, wand_entity_id, abili
 end
 
 local function when_wand_got_increase_draw(spell_entity_id)
-  local wand_entity_id = GetInternalVariableValue(spell_entity_id, 'increase_draw_4.target_wand_entity_id', 'value_int')
+  local wand_entity_id = GetInternalVariableValue(spell_entity_id, INCREASE_DRAW.VARIABLE_KEYS.REIGSTERED_WAND_ENTITY, "value_int")
   if wand_entity_id == nil then
     wand_entity_id = EntityGetParent(spell_entity_id)
-    AddNewInternalVariable(spell_entity_id, 'increase_draw_4.target_wand_entity_id', 'value_int', wand_entity_id)
+    AddNewInternalVariable(spell_entity_id, INCREASE_DRAW.VARIABLE_KEYS.REIGSTERED_WAND_ENTITY, "value_int", wand_entity_id)
   end
 
   local ability_component = get_gun_config_actions_per_round_ability_component(wand_entity_id)
@@ -68,7 +75,7 @@ local function when_wand_got_increase_draw(spell_entity_id)
     return
   end
 
-  local already_increased_actions_per_round = EntityHasTag(spell_entity_id, 'enabled_increase_draw_4')
+  local already_increased_actions_per_round = EntityHasTag(spell_entity_id, INCREASE_DRAW.EXECUTED_MARK_TAG)
   if already_increased_actions_per_round then
     return
   end
@@ -77,7 +84,7 @@ local function when_wand_got_increase_draw(spell_entity_id)
 end
 
 local function when_let_go_of_wand(spell_entity_id)
-  local wand_entity_id = GetInternalVariableValue(spell_entity_id, 'increase_draw_4.target_wand_entity_id', 'value_int')
+  local wand_entity_id = GetInternalVariableValue(spell_entity_id, INCREASE_DRAW.VARIABLE_KEYS.REIGSTERED_WAND_ENTITY, "value_int")
 
   local is_wand_not_registerd = wand_entity_id == nil
   if is_wand_not_registerd then
