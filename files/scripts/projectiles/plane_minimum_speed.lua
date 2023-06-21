ad083f03bd8947ae25b5f4043f743946403062bf
den3606 <dedendendedenpunn@gmail.com>
2023-06-22 06:04:14 +0900

    feat: planeが攻撃されるようにした/ hp buff

--- files/scripts/projectiles/plane_minimum_speed.lua
+++ files/scripts/projectiles/plane_minimum_speed.lua
@@ -4,11 +4,11 @@ local entity_id = GetUpdatedEntityID()
 
 edit_component(entity_id, "VelocityComponent", function(comp, vars)
   local vel_x, vel_y = ComponentGetValueVector2(comp, "mVelocity")
-  if math.abs(vel_x) < 100 then
+  if math.abs(vel_x) < 90 then
     if vel_x > 0 then
-      vel_x = 100
+      vel_x = 90
     else
-      vel_x = -100
+      vel_x = -90
     end
   end
   vel_x = vel_x
